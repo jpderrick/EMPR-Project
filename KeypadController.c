563da020d1fe1345a68a62ef2339620278b063f9
Jon Derrick <jpderrick@hotmail.co.uk>
2017-01-31 13:03:30 +0000

    Beginning to test the Keypad Controller

--- KeypadController.c
+++ KeypadController.c
@@ -1,49 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <lpc17xx_uart.h>
 #include <lpc17xx_pinsel.h>
 #include <lpc_types.h>
 #include <lpc17xx_i2c.h>
+#include "serial.h"
 
-I2C_M_SETUP_Type KeypadTransferConfig;
+LPC_I2C_TypeDef *I2C = LPC_I2C1;
+I2C_M_SETUP_Type ScreenTransferConfig;
 char lastKeyPressed = "X";
 
-int SetupKeypad(void){
+void setupI2C(void){
 
-	
+	PINSEL_CFG_Type PinCfg;
+	PinCfg.Funcnum = 3;
+	PinCfg.OpenDrain = 0;
+	PinCfg.Pinmode = 0;
+	PinCfg.Portnum = 0;
+	PinCfg.Pinnum = 0;
+
+	    //Configure P0.0
+	PINSEL_ConfigPin(&PinCfg);
+
+	    //Configure P0.1
+	PinCfg.Pinnum = 1;
+	PINSEL_ConfigPin(&PinCfg);
+
+
+	I2C_Init(I2C, 100000);
+	I2C_Cmd(I2C,ENABLE);
+
 }
-
 int listenForKey(void){
 	//Just poll the keyboard, waiting for a key to be pressed, 
 	//return 1 if a key has been pressed
-	while(getLastKeyPressed() == "X"){
+	while(lastKeyPressed == "X"){
 		//Loop through each column
 		for(int i = 1;i<=4;i++){
 			KeypadTransferConfig.rx_length = 1;
+			uint8_t buffer[1] = {};
 			KeypadTransferConfig.rx_data = buffer;
 			enableColumn(i);
 			I2C_MasterTransferData(I2C, &TransferConfig, I2C_TRANSFER_POLLING);
+			lastKeyPressed = bufferToChar(buffer[0]);
 		}
+	}
+	return 1;
+}	
+char getKeyPressed(void){
+
+	return lastKeyPressed;
+
+}
+
+int isLastKeyPressed(char key){
+
+	if(key == lastKeyPressed){
+		return 1;
+	}else {
+		return 0;
 	}
 }
 
-char getKeyPressed(void){
-
-
-}
-
-int isKeyPressed(char key){
-
-}
-
-void setLastKeyPressed(void){
-
-}
-
-char getLastKeyPressed(void){
-	return lastKeyPressed;
-}
 char bufferToChar(char buffer){
 
 		if(buffer == 0xB7){
@@ -110,11 +128,66 @@ void enableColumn(int column){
 
 void main(void){
 	setupI2C();
-
+	serial_init();
 	if(SetupKeypad() == 0){
 		//There's a problem witht he keypad setting up
+		write_usb_serial_blocking("Keypad Problem",14);
 	}else{
-
+		write_usb_serial_blocking("Keypad Setup",12);
+		listenForKey();
 	}
 
+}
+
+// Read options
+int read_usb_serial_none_blocking(char *buf,int length)
+{
+	return(UART_Receive((LPC_UART_TypeDef *)LPC_UART0, (uint8_t *)buf, length, NONE_BLOCKING));
+}
+
+// Write options
+int write_usb_serial_blocking(char *buf,int length)
+{
+	return(UART_Send((LPC_UART_TypeDef *)LPC_UART0,(uint8_t *)buf,length, BLOCKING));
+}
+// init code for the USB serial line
+void serial_init(void)
+{
+	UART_CFG_Type UARTConfigStruct;			// UART Configuration structure variable
+	UART_FIFO_CFG_Type UARTFIFOConfigStruct;	// UART FIFO configuration Struct variable
+	PINSEL_CFG_Type PinCfg;				// Pin configuration for UART
+	/*
+	 * Initialize UART pin connect
+	 */
+	PinCfg.Funcnum = 1;
+	PinCfg.OpenDrain = 0;
+	PinCfg.Pinmode = 0;
+	// USB serial first
+	PinCfg.Portnum = 0;
+	PinCfg.Pinnum = 2;
+	PINSEL_ConfigPin(&PinCfg);
+	PinCfg.Pinnum = 3;
+	PINSEL_ConfigPin(&PinCfg);
+		
+	/* Initialize UART Configuration parameter structure to default state:
+	 * - Baudrate = 9600bps
+	 * - 8 data bit
+	 * - 1 Stop bit
+	 * - None parity
+	 */
+	UART_ConfigStructInit(&UARTConfigStruct);
+	/* Initialize FIFOConfigStruct to default state:
+	 * - FIFO_DMAMode = DISABLE
+	 * - FIFO_Level = UART_FIFO_TRGLEV0
+	 * - FIFO_ResetRxBuf = ENABLE
+	 * - FIFO_ResetTxBuf = ENABLE
+	 * - FIFO_State = ENABLE
+	 */
+	UART_FIFOConfigStructInit(&UARTFIFOConfigStruct);
+	// Built the basic structures, lets start the devices/
+	// USB serial
+	UART_Init((LPC_UART_TypeDef *)LPC_UART0, &UARTConfigStruct);		// Initialize UART0 peripheral with given to corresponding parameter
+	UART_FIFOConfig((LPC_UART_TypeDef *)LPC_UART0, &UARTFIFOConfigStruct);	// Initialize FIFO for UART0 peripheral
+	UART_TxCmd((LPC_UART_TypeDef *)LPC_UART0, ENABLE);			// Enable UART Transmit
+	
 }
--- ScreenController.c
+++ ScreenController.c
@@ -5,8 +5,30 @@
 #include <lpc_types.h>
 #include <lpc17xx_i2c.h>
 
+LPC_I2C_TypeDef *I2C = LPC_I2C1;
 I2C_M_SETUP_Type ScreenTransferConfig;
 
+void setupI2C(void){
+
+	PINSEL_CFG_Type PinCfg;
+	PinCfg.Funcnum = 3;
+	PinCfg.OpenDrain = 0;
+	PinCfg.Pinmode = 0;
+	PinCfg.Portnum = 0;
+	PinCfg.Pinnum = 0;
+
+	    //Configure P0.0
+	PINSEL_ConfigPin(&PinCfg);
+
+	    //Configure P0.1
+	PinCfg.Pinnum = 1;
+	PINSEL_ConfigPin(&PinCfg);
+
+
+	I2C_Init(I2C, 100000);
+	I2C_Cmd(I2C,ENABLE);
+
+}
 int SetupScreen(void){
 
 	//The Data to send
